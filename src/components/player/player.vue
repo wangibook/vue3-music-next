47a3dd2265c64d772813590013327a264dc6ab40
dashi <297901039@qq.com>
2021-09-25 17:31:35 +0800

    播放收起和展开动画

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,84 +1,92 @@
 <template>
   <div class="player" v-show="playlist.length">
-    <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <img :src="currentSong.pic" alt="">
-      </div>
-      <div class="top">
-        <div class="back"  @click="goBack">
-          <i class="icon-back"></i>
+    <transition
+      name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave">
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img :src="currentSong.pic" alt="">
         </div>
-        <h1 class="title">{{currentSong.name}}</h1>
-        <h2 class="subtitle">{{currentSong.singer}}</h2>
-      </div>
-      <div 
-        class="middle"
-        @touchstart.prevent="onMiddleTouchStart"
-        @touchmove.prevent="onMiddleTouchMove"
-        @touchend.prevent="onMiddleTouchEnd">
-        <div class="middle-l" :style="middleLStyle">
-          <div class="cd-wrapper">
-            <div class="cd" ref="cdRef">
-              <img class="image" ref="cdImageRef" :class="cdCls" :src="currentSong.pic" alt="">
+        <div class="top">
+          <div class="back"  @click="goBack">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title">{{currentSong.name}}</h1>
+          <h2 class="subtitle">{{currentSong.singer}}</h2>
+        </div>
+        <div 
+          class="middle"
+          @touchstart.prevent="onMiddleTouchStart"
+          @touchmove.prevent="onMiddleTouchMove"
+          @touchend.prevent="onMiddleTouchEnd">
+          <div class="middle-l" :style="middleLStyle">
+            <div class="cd-wrapper" ref="cdWrapperRef">
+              <div class="cd" ref="cdRef">
+                <img class="image" ref="cdImageRef" :class="cdCls" :src="currentSong.pic" alt="">
+              </div>
+            </div>
+            <div class="playing-lyric-wrapper">
+              <div class="playing-lyric">{{playingLyric}}</div>
             </div>
           </div>
-          <div class="playing-lyric-wrapper">
-            <div class="playing-lyric">{{playingLyric}}</div>
-          </div>
-        </div>
-        <scroll class="middle-r" ref="lyricScrollRef" :style="middleRStyle">
-          <div class="lyric-wrapper">
-            <div 
-              ref="lyricListRef" 
-              v-if="currentLyric">
-              <p 
-                class="text"
-                :class="{'current': currentLineNum ===index}"
-                v-for="(item,index) in currentLyric.lines" 
-                :key="index">
-                {{item.txt}}
-              </p>
+          <scroll class="middle-r" ref="lyricScrollRef" :style="middleRStyle">
+            <div class="lyric-wrapper">
+              <div 
+                ref="lyricListRef" 
+                v-if="currentLyric">
+                <p 
+                  class="text"
+                  :class="{'current': currentLineNum ===index}"
+                  v-for="(item,index) in currentLyric.lines" 
+                  :key="index">
+                  {{item.txt}}
+                </p>
+              </div>
+              <div class="pure-music" v-show="pureMusicLyric">
+                <p>{{pureMusicLyric}}</p>
+              </div>
             </div>
-            <div class="pure-music" v-show="pureMusicLyric">
-              <p>{{pureMusicLyric}}</p>
+          </scroll>
+        </div>
+        <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot" :class="{'active':currentShow==='cd'}"></span>
+            <span class="dot" :class="{'active':currentShow==='lyric'}"></span>
+          </div>
+          <div class="progress-wrapper">
+            <span class="time time-l">{{formatTime(currentTime)}}</span>
+            <div class="progress-bar-wrapper">
+              <progressBar
+                ref="barRef"
+                :progress="progress"
+                @progress-changing="onProgressChanging"
+                @progress-changed="onProgressChanged" />
             </div>
+            <span class="time time-r">{{formatTime(currentSong.duration)}}</span>
           </div>
-        </scroll>
-      </div>
-      <div class="bottom">
-        <div class="dot-wrapper">
-          <span class="dot" :class="{'active':currentShow==='cd'}"></span>
-          <span class="dot" :class="{'active':currentShow==='lyric'}"></span>
-        </div>
-        <div class="progress-wrapper">
-          <span class="time time-l">{{formatTime(currentTime)}}</span>
-          <div class="progress-bar-wrapper">
-            <progressBar
-              :progress="progress"
-              @progress-changing="onProgressChanging"
-              @progress-changed="onProgressChanged" />
-          </div>
-          <span class="time time-r">{{formatTime(currentSong.duration)}}</span>
-        </div>
-        <div class="operators">
-          <div class="icon i-left">
-            <i @click="changeMode" :class="modeIcon"></i>
-          </div>
-          <div class="icon i-left" :class="disableCls">
-            <i @click="prev" class="icon-prev"></i>
-          </div>
-          <div class="icon i-center" :class="disableCls">
-            <i @click="togglePlay" :class="playIcon"></i>
-          </div>
-          <div class="icon i-right" :class="disableCls">
-            <i @click="next" class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i @click="toggleFavorite(currentSong)" :class="getFavoriteIcon(currentSong)"></i>
+          <div class="operators">
+            <div class="icon i-left">
+              <i @click="changeMode" :class="modeIcon"></i>
+            </div>
+            <div class="icon i-left" :class="disableCls">
+              <i @click="prev" class="icon-prev"></i>
+            </div>
+            <div class="icon i-center" :class="disableCls">
+              <i @click="togglePlay" :class="playIcon"></i>
+            </div>
+            <div class="icon i-right" :class="disableCls">
+              <i @click="next" class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i @click="toggleFavorite(currentSong)" :class="getFavoriteIcon(currentSong)"></i>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
 
     <!-- 底部的迷你播放器 -->
     <mini-player 
@@ -98,13 +106,14 @@
 </template>
 
 <script>
-import { ref,computed, watch } from 'vue'
+import { ref,computed, watch,nextTick } from 'vue'
 import { useStore } from 'vuex'
 import useMode from './use-mode'
 import useFavorite from './use-favorite'
 import useCd from './use-cd'
 import useLyric from './use-lyric'
 import useMiddleInteractive from './use-middle-interactive'
+import useAnimation from './use-animation'
 import progressBar from './progress-bar'
 import miniPlayer from './mini-player'
 import scroll from '@/components/scroll/scroll'
@@ -120,6 +129,7 @@ export default {
   setup() {
     // data
     const audioRef = ref(null)
+    const barRef = ref(null)
     const songReady = ref(false)
     const currentTime = ref(1550)
     let progressChanging = false
@@ -138,6 +148,7 @@ export default {
     const { cdCls,cdRef, cdImageRef } = useCd()
     const { currentLyric,currentLineNum,lyricScrollRef,lyricListRef,pureMusicLyric,playingLyric,playLyric,stopLyric } = useLyric({songReady,currentTime})
     const { currentShow,middleLStyle,middleRStyle,onMiddleTouchStart,onMiddleTouchMove,onMiddleTouchEnd } = useMiddleInteractive()
+    const { cdWrapperRef,enter, afterEnter, leave, afterLeave } = useAnimation()
     
     // computed
     const playIcon = computed(() => {
@@ -174,6 +185,14 @@ export default {
       } else {
         audioEl.pause()
         stopLyric()
+      }
+    })
+    
+    // mini播放器收缩的时候，fullScreen变为了false，进度条就丢失了，当打开时重新请求，在dom改变之后
+    watch(fullScreen,async (newFullScreen) => {
+      if(newFullScreen) {
+        await nextTick()
+        barRef.value.setOffset(progress.value)
       }
     })
 
@@ -279,6 +298,7 @@ export default {
 
     return {
       audioRef,
+      barRef,
       fullScreen,
       currentSong,
       playlist,
@@ -322,7 +342,13 @@ export default {
       middleRStyle,
       onMiddleTouchStart,
       onMiddleTouchMove,
-      onMiddleTouchEnd
+      onMiddleTouchEnd,
+      // use-animation
+      cdWrapperRef,
+      enter, 
+      afterEnter, 
+      leave, 
+      afterLeave
     }
   }
 }
@@ -534,6 +560,21 @@ export default {
         .icon-favorite {
           color: $color-sub-theme;
         }
+      }
+    }
+    &.normal-enter-active, &.normal-leave-active {
+      transition: all .6s;
+      .top, .bottom {
+        transition: all .6s ease-out;
+      }
+    }
+    &.normal-enter-from, &.normal-leave-to {
+      opacity: 0;
+      .top {
+        transform: translate3d(0, -100px, 0);
+      }
+      .bottom {
+        transform: translate3d(0, 100px, 0)
       }
     }
   }
---
+++ src/components/player/use-animation.js
@@ -0,0 +1,83 @@
+import { ref } from 'vue'
+import animations from 'create-keyframe-animation'
+
+export default function useAnimation() {
+  const cdWrapperRef = ref(null)
+
+  // cd从隐藏到显示的动画，
+  const enter = (el,done) => {
+    const { x, y, scale } = getPosAndScale()
+
+    let animation = {
+      0: {
+        transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+      },
+      100: {
+        transform: 'translate3d(0, 0, 0) scale(1)'
+      }
+    }
+    // 动画的一些配置
+    animations.registerAnimation({
+      name: 'move',
+      animation,
+      presets: {
+        duration: 600,
+        easing: 'ease-out'
+      }
+    })
+    // 运行动画
+    animations.runAnimation(cdWrapperRef.value,'move',done)
+  }
+
+  const afterEnter = () => {
+    //运行完动画之后，注销掉动画
+    animations.unregisterAnimation('move')
+    const cdWrapperEl = cdWrapperRef.value
+    cdWrapperEl.style.animation = ''
+  }
+
+  // cd从显示到隐藏的动画
+  const leave = (el,done) => {
+    const { x, y, scale } = getPosAndScale()
+    const cdWrapperEl = cdWrapperRef.value
+    cdWrapperEl.style.transition = 'all .6s'
+    // 这里我们只要直接移动变小就可以了
+    cdWrapperEl.style.transform = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+    cdWrapperEl.addEventListener('transitionend',() => {
+      done()
+    })
+  }
+
+  const afterLeave = () => {
+    const cdWrapperEl = cdWrapperRef.value
+    cdWrapperEl.style.transition = ''
+    cdWrapperEl.style.transform = ''
+  }
+
+  const getPosAndScale = () => {
+    // 左下角小图片的宽度
+    const targetWidth = 40
+    const paddingLeft = 40
+    const paddingBottom = 30
+    const paddingTop = 80
+    // cd宽度
+    const width = window.innerWidth * 0.8
+    const x = -(window.innerWidth / 2 - paddingLeft)
+    const y = window.innerHeight - paddingTop - width/2 - paddingBottom
+    const scale = targetWidth / width
+
+    return {
+      x,
+      y,
+      scale
+    }
+  }
+
+  return {
+    cdWrapperRef,
+    enter,
+    afterEnter,
+    leave,
+    afterLeave
+  }
+}
